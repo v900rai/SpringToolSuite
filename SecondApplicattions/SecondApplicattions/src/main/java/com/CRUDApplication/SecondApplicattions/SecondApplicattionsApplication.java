43b7b7c88177de422fa5bd7dcbd3b9f4eaed3081
vishal rai <vishalrai10797@gmail.com>
2024-07-25 21:59:58 +0530

    my second project

---
+++ SecondApplicattions/SecondApplicattions/src/main/java/com/CRUDApplication/SecondApplicattions/SecondApplicattionsApplication.java
@@ -0,0 +1,14 @@
+package com.CRUDApplication.SecondApplicattions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondApplicattionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondApplicattionsApplication.class, args);
+		System.out.println("Spring Program RunSuccessfully");
+	}
+
+}
---
+++ SecondApplicattions/SecondApplicattions/src/test/java/com/CRUDApplication/SecondApplicattions/SecondApplicattionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRUDApplication.SecondApplicattions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicattionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
