2ea1bb0a8173eaa6f36d15840bf5df89e7196525
vishal rai <vishalrai10797@gmail.com>
2024-07-25 21:43:52 +0530

    Spring Projects

---
+++ springBootProject3/springBootProject3/src/test/java/in/smartprogramming/springBootProject3/SpringBootProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.smartprogramming.springBootProject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
